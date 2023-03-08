84da3c5266f59273911f457ac5f6eebb19a14bf2
Oskar Weber <s189424@student.pg.edu.pl>
2023-03-08 22:15:55 +0100

    x

--- main.py
+++ main.py
@@ -1,61 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import copy
 
 
 class DE:
     def __init__(self, initial_cond, boundary_cond_1, boundary_cond_2):
-        self.initial_cond = initial_cond
-        self.boundary_cond_1 = boundary_cond_1
-        self.boundary_cond_2 = boundary_cond_2
+        self.condition_list = {'initial_cond': initial_cond, 'boundary_cond_1': boundary_cond_1,
+                               'boundary_cond_2': boundary_cond_2}
 
-    def euler_method(self, t: int, x: int, t_step: float, x_step: float, t_start: int, x_start: int):
+    @staticmethod
+    def initialize_conditions(condition):
+        if isinstance(condition, (float, int)):
+            return lambda *args: condition
+        else:
+            return lambda x: condition(x)
+
+    def euler_method(self, t: int, x: int, t_step: float, x_step: float) -> list[float]:
         t_steps = int((t/t_step)+1)
         x_steps = int((x/x_step)+1)
 
+        l_conditions = {key: DE.initialize_conditions(condition)
+                        for key, condition in self.condition_list.items()}
         v = np.zeros(x_steps)
-        if isinstance(self.initial_cond, (float, int)):
-            u = np.full(x_steps, float(self.initial_cond))
-        else:
-            u = [float(self.initial_cond(i * x_step)) for i in range(x_steps)]
-        for j in range(t_start, t_steps):
-            if isinstance(self.boundary_cond_1, (float, int)):
-                v[0] = float(self.boundary_cond_1)
-            else:
-                v[0] = self.boundary_cond_1(j * t_step)
-            if isinstance(self.boundary_cond_2, (float, int)):
-                v[-1] = float(self.boundary_cond_2)
-            else:
-                v[-1] = self.boundary_cond_2(j * t_step)
-            for i in range(x_start, x_steps-1):
+        u = [float(l_conditions['initial_cond'](i * x_step)) for i in range(x_steps)]
+        for j in range(1, t_steps):
+            v[0] = l_conditions['boundary_cond_1'](j * t_step)
+            v[-1] = l_conditions['boundary_cond_2'](j * t_step)
+            for i in range(1, x_steps-1):
                 v[i] = u[i] - ((t_step / (2 * x_step)) * (u[i+1] - u[i-1]))
             u = v[:]
         return u
 
-    def mod_euler_method(self, t: int, x: int, t_step: float, x_step: float, t_start: int, x_start: int):
+    def mod_euler_method(self, t: int, x: int, t_step: float, x_step: float) -> list[float]:
         t_steps = int((t/t_step)+1)
         x_steps = int((x/x_step)+1)
 
+        l_conditions = {key: DE.initialize_conditions(condition)
+                        for key, condition in self.condition_list.items()}
         intermediate, v = np.zeros(x_steps), np.zeros(x_steps)
-        if isinstance(self.initial_cond, (float, int)):
-            u = np.full(x_steps, float(self.initial_cond))
-        else:
-            u = [float(self.initial_cond(i * x_step)) for i in range(x_steps)]
-        for j in range(t_start, t_steps):
-            if isinstance(self.boundary_cond_1, (float, int)):
-                intermediate[0], v[0] = float(self.boundary_cond_1), float(self.boundary_cond_1)
-            else:
-                intermediate[0] = self.boundary_cond_1((j-0.5) * t_step)
-                v[0] = self.boundary_cond_1(j * t_step)
-            if isinstance(self.boundary_cond_2, (float, int)):
-                intermediate[-1], v[-1] = float(self.boundary_cond_2), float(self.boundary_cond_2)
-            else:
-                intermediate[-1] = self.boundary_cond_2((j-0.5) * t_step)
-                v[-1] = self.boundary_cond_2(j * t_step)
-            for i in range(x_start, x_steps - 1):
+        u = [float(l_conditions['initial_cond'](i * x_step)) for i in range(x_steps)]
+        for j in range(1, t_steps):
+            intermediate[0] = l_conditions['boundary_cond_1']((j-0.5) * t_step)
+            v[0] = l_conditions['boundary_cond_1'](j * t_step)
+            intermediate[-1] = l_conditions['boundary_cond_2']((j-0.5) * t_step)
+            v[-1] = l_conditions['boundary_cond_2'](j * t_step)
+            for i in range(1, x_steps - 1):
                 intermediate[i] = u[i] - (t_step / (4 * x_step)) * (u[i + 1] - u[i - 1])
-            for i in range(x_start, x_steps - 1):
+            for i in range(1, x_steps - 1):
                 v[i] = u[i] - (t_step / (2 * x_step)) * (intermediate[i + 1] - intermediate[i - 1])
             u = v[:]
         return u
@@ -65,28 +56,28 @@ def euler_2_bound_cond(x: float) -> float:
     return math.exp(-(pow((x - 10), 2)))
 
 
+def plot(data: dict, x_axis=None) -> None:
+    lines = [None] * len(data)
+    fig, (axs) = plt.subplots(1, len(data), figsize=(15, 7))
+    for n, value in enumerate(data.items()):
+        lines[n], = axs[n].plot(x_axis, value[1])
+        axs[n].locator_params(nbins=5)
+        axs[n].set_title(value[0])
+    del x_axis
+    plt.show()
+
+
 if __name__ == '__main__':
-    time = 5 # int
-    displacement = 20  # int
+    TIME, DISPLACEMENT = 5, 20  # int, int
+    T_STEP, X_STEP = 0.01, 0.1  # float, float
+    T_START, X_START = 1, 1  # int, int
+    X_AXIS = np.arange(0, ((DISPLACEMENT/X_STEP)+1)/10, 0.1)
     equation_1 = DE(0, math.sin, 0)
     equation_2 = DE(euler_2_bound_cond, 0, 0)
-    x_axis = np.arange(201)
+    TO_PLOT = {'euler_u_1': equation_1.euler_method(TIME, DISPLACEMENT, T_STEP, X_STEP),
+    'euler_u_2': equation_2.euler_method(TIME, DISPLACEMENT, T_STEP, X_STEP),
+    'mod_euler_u_1': equation_1.mod_euler_method(TIME, DISPLACEMENT, T_STEP, X_STEP),
+    'mod_euler_u_2': equation_2.mod_euler_method(TIME, DISPLACEMENT, T_STEP, X_STEP)}
 
-    euler_u_1 = equation_1.euler_method(time, displacement, 0.01, 0.1, 1, 1)
-    euler_u_2 = equation_2.euler_method(time, displacement, 0.01, 0.1, 1, 1)
-
-    mod_euler_u_1 = equation_1.mod_euler_method(time, displacement, 0.01, 0.1, 1, 1)
-    mod_euler_u_2 = equation_2.mod_euler_method(time, displacement, 0.01, 0.1, 1, 1)
-
-    fig, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4, figsize=(20, 7))
-    line1, = ax1.plot(x_axis, euler_u_1, color='green')
-    line2, = ax2.plot(x_axis, euler_u_2, color='red')
-    line3, = ax3.plot(x_axis, mod_euler_u_1, color='blue')
-    line4, = ax4.plot(x_axis, mod_euler_u_2, color='yellow')
-    ax1.set_title("euler_u_1")
-    ax2.set_title("euler_u_2")
-    ax3.set_title("mod_euler_u_1")
-    ax4.set_title("mod_euler_u_2")
-
-    plt.show()
+    plot(TO_PLOT, X_AXIS)
 
